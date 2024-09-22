50a66c1536d80744ad32edfc65193c3a7bd8e100
BdNWoG <billy.gao@hotmail.com>
2024-09-21 20:22:55 -0700

    kinda

--- portie/components/ui/BentoGrid.tsx
+++ portie/components/ui/BentoGrid.tsx
@@ -1,55 +1,87 @@
 import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./BackgroundGradient";
 
 export const BentoGrid = ({
-  className,
-  children,
+    className,
+    children,
 }: {
-  className?: string;
-  children?: React.ReactNode;
+    className?: string;
+    children?: React.ReactNode;
 }) => {
-  return (
-    <div
-      className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
+    return (
+        <div
+        className={cn(
+            "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
+            className
+        )}
+        >
+        {children}
+        </div>
+    );
 };
 
 export const BentoGridItem = ({
-  className,
-  title,
-  description,
-  header,
-  icon,
+    className,
+    title,
+    description,
+    id, 
+    img, 
+    imgClassName,
+    titleClassName,
+    spareImg
 }: {
-  className?: string;
-  title?: string | React.ReactNode;
-  description?: string | React.ReactNode;
-  header?: React.ReactNode;
-  icon?: React.ReactNode;
-  id: number;
+    className?: string;
+    title?: string | React.ReactNode;
+    description?: string | React.ReactNode;
+    id?: number;
+    img?: string;
+    imgClassName?: string;
+    titleClassName?: string;
+    spareImg?: string;
 }) => {
-  return (
-    <div
-      className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
-        className
-      )}
-    >
-      {header}
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
+    return (
+        <div
+        className={cn(
+            "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+            className
+        )} style={{
+            background: 'rgb(4, 7, 29)', 
+            backgroundColor:
+            "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+        }}
+        >
+        <div className={`${id === 6} && 'flex justify-center h-full`}>
+            <div className="h-full w-full absolute">
+            <img
+                src={img}
+                alt={img}
+                className={cn(
+                "object-cover object-center",
+                imgClassName
+                )}
+            />
+            </div>
+            <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+                {spareImg && <img
+                src={spareImg}
+                alt={spareImg}
+                className="object-cover object-center w-full h-full"
+                />}
+            </div>
+            {id === 6 && (
+                <BackgroundGradientAnimation>
+                    <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+                </BackgroundGradientAnimation>
+            )}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
+        <div className="group-hover/bento:translate-x-2 transition duration-200">
+            <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+            {title}
+            </div>
+            <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+            {description}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
